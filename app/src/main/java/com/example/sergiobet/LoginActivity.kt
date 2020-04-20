3d6676656b068003a3897837319278f6b0b6ccf3
Sergio Campos da Fonseca <sergiofonseca@iheartmedia.com>
2020-04-20 12:10:12 -0300

    Add preference

--- app/src/main/java/com/example/sergiobet/LoginActivity.kt
+++ app/src/main/java/com/example/sergiobet/LoginActivity.kt
@@ -1,22 +1,57 @@
 package com.example.sergiobet
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sergiobet.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+
+    private lateinit var binding: ActivityLoginBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
 
-        findViewById<Button>(R.id.login_button).setOnClickListener {
+        inflateBinding()
+        initLoginName()
+
+        binding.loginButton.setOnClickListener {
+
+            if (isPasswordValid()) return@setOnClickListener
+
             startActivity(
                 Intent(this, HomeActivity::class.java)
             )
         }
     }
+
+    private fun isPasswordValid(): Boolean {
+        if (binding.loginPassword.text.toString() != "123") {
+            binding.loginPasswordLayout.error = "Password is wrong"
+            return true
+        }
+
+        binding.loginPasswordLayout.error = null;
+        return false
+    }
+
+    private fun initLoginName() {
+        binding.loginName.setText(getPreferences(Context.MODE_PRIVATE).getString(LOGIN_NAME, ""))
+
+        binding.loginName.setOnFocusChangeListener { v, hasFocus ->
+            getPreferences(Context.MODE_PRIVATE).edit()
+                .putString(LOGIN_NAME, binding.loginName.text.toString()).apply()
+        }
+    }
+
+    private fun inflateBinding() {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
 }
+
+private const val LOGIN_NAME = "LOGIN_NAME"
+
